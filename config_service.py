38c11ecfb55127c60e04eaecfbe5ed258e3c8255
FHU-yezi <yehaowei20060411@qq.com>
2022-01-13 12:10:44 +0800

    feat：完成配置服务

---
+++ config_service.py
@@ -0,0 +1,24 @@
+from yaml import dump, load, SafeLoader
+from typing import Dict
+
+DEFALUT_CONFIG = {
+    "messae_service": {
+        "app_id": "",
+        "app_secret": "",
+        "email": ""
+    }
+}
+
+
+def CreateDefaultConfig() -> None:
+    with open("config.yaml", "w", encoding="utf-8") as f:
+        dump(DEFALUT_CONFIG, f, indent=4, allow_unicode=True)
+
+
+def GetConfig() -> Dict:
+    try:
+        with open("config.yaml", "r", encoding="utf-8") as f:
+            return load(f, SafeLoader)
+    except FileNotFoundError:  # 日志文件不存在
+        CreateDefaultConfig()
+        return GetConfig()
