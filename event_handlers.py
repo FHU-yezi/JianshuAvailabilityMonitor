fdffbc4f7a02ce6c5dab29c883ed5e278434f486
FHU-yezi <yehaowei20060411@qq.com>
2022-01-13 21:11:10 +0800

    feat：支持发送服务重新上线消息

--- event_handlers.py
+++ event_handlers.py
@@ -1,20 +1,30 @@
-from log_service import AddMonitorLog, AddRunLog
-from message_service import SendFeishuMonitorFailureMessage, SendFeishuMessage
+from log_service import AddMonitorLog, AddRunLog, IsFailedUntilNow
+from message_service import SendFeishuMonitorFailureMessage, SendFeishuReAvailableMessage
 from config_service import GetConfig
 
 
 def JobExecutedSuccessfully(event) -> None:
     monitor_name = event.job_id
     success, status_code, message = event.retval
-    
-    AddMonitorLog(monitor_name, success, status_code, message)
+
     if not success:
         print(f"任务出错：{message}")
-        SendFeishuMonitorFailureMessage(GetConfig()["message_service"]["app_id"],
-                                        GetConfig()["message_service"]["app_secret"],
-                                        GetConfig()["message_service"]["email"],
-                                        monitor_name, status_code, message)
-        AddRunLog(3, f"发送了新的告警信息，发生错误的任务：{monitor_name}，错误码：{status_code}，错误信息：{message}")
+        if IsFailedUntilNow(monitor_name):
+            # 告警信息已经发送过
+            AddRunLog(4, f"跳过告警信息发送，因为 {monitor_name} 的告警信息已经发送过")
+        else:
+            SendFeishuMonitorFailureMessage(GetConfig()["message_service"]["app_id"],
+                                            GetConfig()["message_service"]["app_secret"],
+                                            GetConfig()["message_service"]["email"],
+                                            monitor_name, status_code, message)
+            AddRunLog(3, f"发送了新的告警信息，发生错误的任务：{monitor_name}，错误码：{status_code}，错误信息：{message}")
+    else:  # 任务执行成功
+        if IsFailedUntilNow(monitor_name):  # 服务恢复
+            SendFeishuReAvailableMessage(GetConfig()["message_service"]["app_id"],
+                                         GetConfig()["message_service"]["app_secret"],
+                                         GetConfig()["message_service"]["email"],
+                                         monitor_name)
+    AddMonitorLog(monitor_name, success, status_code, message)
 
 
 def JobExecutedFailure(event) -> None:
@@ -22,9 +32,14 @@ def JobExecutedFailure(event) -> None:
     success = False
     status_code = -2
     message = str(event.exception)
+
+    if IsFailedUntilNow(monitor_name):
+        # 告警信息已经发送过
+        AddRunLog(4, f"跳过告警信息发送，因为 {monitor_name} 的告警信息已经发送过")
+    else:
+        SendFeishuMonitorFailureMessage(GetConfig()["message_service"]["app_id"],
+                                        GetConfig()["message_service"]["app_secret"],
+                                        GetConfig()["message_service"]["email"],
+                                        monitor_name, status_code, message)
+        AddRunLog(3, f"发送了新的告警信息，发生错误的任务：{monitor_name}，错误码：{status_code}，错误信息：{message}")
     AddMonitorLog(monitor_name, success, status_code, message)
-    SendFeishuMonitorFailureMessage(GetConfig()["message_service"]["app_id"],
-                                    GetConfig()["message_service"]["app_secret"],
-                                    GetConfig()["message_service"]["email"],
-                                    monitor_name, status_code, message)
-    AddRunLog(3, f"发送了新的告警信息，发生错误的任务：{monitor_name}，错误码：{status_code}，错误信息：{message}")
--- message_service.py
+++ message_service.py
@@ -1,4 +1,5 @@
 from typing import Union
+from datetime import datetime
 
 from httpx import post as httpx_post
 
@@ -36,7 +37,7 @@ def SendFeishuMonitorFailureMessage(app_id: str, app_secret: str, email: str, ta
             },
             "elements": [{
                 "tag": "markdown",
-                "content": f"**任务名称：**{task_name}\n**状态码：**{status_code}\n**错误信息：**{message}"
+                "content": f"**时间：**{datetime.now()}\n**任务名称：**{task_name}\n**状态码：**{status_code}\n**错误信息：**{message}"
             }]
         }
     }
@@ -46,3 +47,33 @@ def SendFeishuMonitorFailureMessage(app_id: str, app_secret: str, email: str, ta
         AddRunLog(1, f"向{email}发送监控预警飞书消息失败，错误码：{response.json()['code']}")
     else:
         AddRunLog(3, f"向{email}发送监控预警飞书消息成功，消息代码：{response.json()['data']['message_id']}")
+
+
+def SendFeishuReAvailableMessage(app_id: str, app_secret: str, email: str, task_name: str) -> None:
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    data_to_get_token = {"app_id": app_id, "app_secret": app_secret}
+    headers["Authorization"] = "Bearer " + httpx_post("https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal", headers=headers, json=data_to_get_token).json()["tenant_access_token"]
+
+    data_to_send_message = {
+        "email": email,
+        "msg_type": "interactive",
+        "card": {
+            "header": {
+                "title": {
+                    "tag": "plain_text",
+                    "content": "服务恢复"
+                },
+                "template": "green"
+            },
+            "elements": [{
+                "tag": "markdown",
+                "content": f"**时间：**{datetime.now()}\n**任务名称：**{task_name}"
+            }]
+        }
+    }
+
+    response = httpx_post("https://open.feishu.cn/open-apis/message/v4/send/", headers=headers, json=data_to_send_message)
+    if response.json()["code"] != 0:
+        AddRunLog(1, f"向{email}发送服务恢复飞书消息失败，错误码：{response.json()['code']}")
+    else:
+        AddRunLog(3, f"向{email}发送服务恢复飞书消息成功，消息代码：{response.json()['data']['message_id']}")
